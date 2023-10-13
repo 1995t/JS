aad569fe23b300977592ec629a9198c710aa4e7e
tarun <tarun.bugata@gmail.com>
2023-10-13 14:30:32 +0530

    datatype summary

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,25 @@
+/*
+ Primitive Datatypes (Call by value)
+  String
+  Number
+  Boolean
+  null
+  Undefined
+  BigInt
+  Symbol
+*/
+
+/*
+Reference - Non Primitive (Call by reference)
+ Arrays, Objects, Functions
+*/
+
+// JavaScript - Dynamically typed language
+
+let id = Symbol("123");
+let anotherId = Symbol("123");
+
+// console.log(id);
+// console.log(anotherId);
+
+console.log(id == anotherId);
