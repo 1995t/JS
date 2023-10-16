5b420a4e6ee19c5abcb342064ec1d00260c24e22
tarun <tarun.bugata@gmail.com>
2023-10-16 12:06:46 +0530

    Added to do list and math

---
+++ basics/math.js
@@ -0,0 +1,8 @@
+// console.log(Math.random());
+
+// console.log((Math.round(Math.random()*10)+1));
+
+let min = 50;
+let max = 100;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
---
+++ basics/todo.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+let value = [];
+let i =0;
+do {
+  
+ let input = prompt(`Enter a input`);
+console.log(typeof(input));
+//  if(input)
+//  {
+//     value.push(input);
+//  } 
+i++;
+}while(i==1);
