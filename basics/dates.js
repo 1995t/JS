43324044f089177253ade7244e095eaea7238bbd
tarun <tarun.bugata@gmail.com>
2023-10-16 21:37:01 +0530

    Added dates and objects

---
+++ basics/dates.js
@@ -0,0 +1,5 @@
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.toString());
+// console.log(newDate.toDateString());
+console.log(newDate.toISOString());
---
+++ basics/objects.js
@@ -0,0 +1,9 @@
+const car = {
+    model:"SUV",
+    mileage: 100,
+    distanceTravelled: 10000  
+}
+// console.log(car[`model`]);
+const carObjectKeys = Object.keys(car);
+// console.log(carObjectKeys);
+console.log(Object.values(car));
