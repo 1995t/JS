40ecae257c91f4e0afb7050eedbde45e49a06ebc
tarun <tarun.bugata@gmail.com>
2023-11-22 12:02:50 +0530

    events

---
+++ basics/string.js
@@ -0,0 +1,12 @@
+let name = 'Tarun';
+let score = '100';
+
+//console.log(`My name is ${name} and score is ${score}.`);
+
+let gameName = new String('Read Dead Redemption');
+
+//console.log(gameName);
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('ad'));
+// console.log(gameName.trim());
